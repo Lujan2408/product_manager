5c8437195b58cfb576f328344e19f191fb19d03d
Jose David <lujan9495@gmail.com>
2025-07-01 10:37:37 -0500

    Add timestamps to Product model and update schema
    - Introduced created_at and updated_at fields to the Product model for tracking creation and modification dates.

--- app/models/products/product.py
+++ app/models/products/product.py
@@ -3,10 +3,14 @@
 # 2. Define the relationships between tables
 # 3. CRUD with the database
 
+from datetime import date
 from sqlmodel import Field, SQLModel, Column, Float
+from sqlalchemy import DateTime as SQLAlchemyDateTime
 
 class Product(SQLModel, table=True): 
   id: int = Field(default=None, primary_key=True)
   name: str = Field(index=True, min_length=3, max_length=255)
   price: float = Field(ge=0, sa_column=Column(Float))
   available: bool = Field(default=True)
+  created_at: date = Field(default_factory=date.today, sa_column=Column(SQLAlchemyDateTime, default=date.today))
+  updated_at: date = Field(default_factory=date.today, sa_column=Column(SQLAlchemyDateTime, default=date.today, onupdate=date.today))
--- app/schemas/product.py
+++ app/schemas/product.py
@@ -5,6 +5,7 @@
 # 4. Data types transform 
 # 5. MUST NOT interact with the database
 
+from datetime import date
 from pydantic import BaseModel, Field
 
 # Schema for creating a product (req)
@@ -19,7 +20,8 @@ class ProductResponse(BaseModel):
   name: str
   price: float
   available: bool
-
+  created_at: date
+  updated_at: date
   # The form_attributes allows the ORM (SQLModel) to convert the data from the database to the data of the response.
   class Config: 
     from_attributes = True
